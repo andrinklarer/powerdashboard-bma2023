0688dfc1ed13175e21088edb8840ca825977add9
jannik-boeppli <jannik.boeppli@noseryoung.ch>
2023-10-27 15:52:21 +0200

    created a template for the politics cards

--- src/pages/politik/index.tsx
+++ src/pages/politik/index.tsx
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
 import Navbar from "~/components/Navbar";
+import PoliticsCard from "~/components/PoliticsCard";
+
+const politicsJson = [
+  {
+    partyComment: "\"....\"",
+    partyImage: "night-mode.svg",
+    partyName: "svp",
+    linkToParty: "https://www.svp.ch",
+    readMoreLink:
+      "svp",
+  }
+];
 
 const politics = () => {
   return (
     <>
       <div className="flex">
-        <Navbar />
         <main className="relative flex-grow">
-          
+          <Navbar />
+          <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-4 justify-center`}>
+          {politicsJson.map((item) => (
+            <PoliticsCard
+              partyComment={item.partyComment}
+              partyImage={item.partyImage}
+              partyName={item.partyName}
+              linkToParty={item.linkToParty}
+              readMoreLink={item.readMoreLink}
+              key={item.partyName}
+            />
+          ))}
+          </div>
         </main>
       </div>
     </>
