0dc6c8d37bb5de2b86582c745a8ad2e0b3d97957
jannik-boeppli <jan04boe@gmail.com>
2023-11-18 02:04:53 +0100

    updated politics card and added card svp, sp, edu
    changed politics card to be similar to the ones on the homepage and added the cards for svp, sp and edu

--- src/components/PoliticsCard.tsx
+++ src/components/PoliticsCard.tsx
@@ -1,5 +1,18 @@
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+
+import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
 import Image from "next/image";
-import * as React from "react";
+import { Separator } from "@radix-ui/react-separator";
+import { AlertTriangle, LineChart, Search, Users } from "lucide-react";
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import Navbar from "../components/Navbar";
 
 interface PoliticsCard {
   linkToParty: string;
@@ -7,6 +20,8 @@ interface PoliticsCard {
   partyName: string;
   partyComment: string;
   readMoreLink: string;
+  partyImageSource: string;
+  partyProgramLink: string;
 }
 
 const PoliticsCard = ({
@@ -15,37 +30,57 @@ const PoliticsCard = ({
   partyName,
   partyComment,
   readMoreLink,
+  partyImageSource,
+  partyProgramLink,
 }: PoliticsCard) => {
   return (
-    <div className="flex justify-center p-5">
-      <div className="flex-grow flex-col rounded-lg border border-gray-200 bg-white p-5 shadow dark:border-gray-700 dark:bg-gray-900">
-        <a href={linkToParty}>
-          <div className="flex items-center justify-center">
-            <Image
-              width={400}
-              height={400}
-              className="rounded-t-lg "
-              src={partyImage}
-              alt={partyName}
-            />
+    <Card className="flex max-h-[520px] max-w-[450px] flex-wrap">
+      <CardHeader className="p-6">
+        <CardTitle className="flex items-center justify-center">
+          <div className="text-center text-2xl font-bold tracking-tight">
+            <a href={linkToParty}>
+              <div className="flex h-56 w-full items-center justify-center overflow-hidden rounded-t-lg">
+                <Image
+                  className="max-h-56 w-auto max-w-full rounded-t-lg"
+                  src={partyImage}
+                  alt={partyName}
+                  width={400} 
+                  height={220}
+                  layout="responsive" 
+                  objectFit="contain" 
+                />
+              </div>
+            </a>
+            {partyName}
           </div>
-          <div className="flex items-center justify-center pt-10">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {partyName}
-            </h5>
-          </div>
-        </a>
-
-        <div className="flex items-center justify-center">
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            {partyComment}
-          </p>
-        </div>
-        <div className="flex items-center justify-center">
-          <a
-            href={readMoreLink}
-            className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
+        </CardTitle>
+        <CardDescription className="text-md text-center">
+          {partyComment}
+        </CardDescription>
+      </CardHeader>
+      <CardFooter className="w-full justify-around pt-0">
+        <Link href={partyProgramLink} className="m-1">
+          <Button>
+            Zum Parteiprogram
+            <svg
+              className="ml-2 h-3.5 w-3.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Button>
+        </Link>
+        <Link href={readMoreLink} className="m-1">
+          <Button>
             Weiter Lesen
             <svg
               className="ml-2 h-3.5 w-3.5"
@@ -62,10 +97,86 @@ const PoliticsCard = ({
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
-          </a>
-        </div>
-      </div>
-    </div>
+          </Button>
+        </Link>
+      </CardFooter>
+    </Card>
+
+    // <div className="flex justify-center p-5">
+    //   <div className="flex-grow flex-col rounded-lg border border-gray-200 bg-white p-5 shadow dark:border-gray-700 dark:bg-gray-900">
+    //     <a href={linkToParty}>
+    //       <div className="flex items-center justify-center">
+    //         <Image
+    //           width={400}
+    //           height={400}
+    //           className="rounded-t-lg "
+    //           src={partyImage}
+    //           alt={partyName}
+    //         />
+    //       </div>
+    //       <div className="flex items-center justify-center pt-10">
+    //         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+    //           {partyName}
+    //         </h5>
+    //       </div>
+    //     </a>
+    //     <div>
+    //     <CardFooter className="items-end pt-0">
+    //       <Link href="/politik">
+    //         <Button>
+    //           Zum Parteiprogram
+    //           <svg
+    //             className="ml-2 h-3.5 w-3.5"
+    //             aria-hidden="true"
+    //             xmlns="http://www.w3.org/2000/svg"
+    //             fill="none"
+    //             viewBox="0 0 14 10"
+    //           >
+    //             <path
+    //               stroke="currentColor"
+    //               stroke-linecap="round"
+    //               stroke-linejoin="round"
+    //               stroke-width="2"
+    //               d="M1 5h12m0 0L9 1m4 4L9 9"
+    //             />
+    //           </svg>
+    //         </Button>
+    //       </Link>
+    //     </CardFooter>
+
+    //     <CardFooter className="items-end pt-0">
+    //       <Link href="/politik">
+    //         <Button>
+    //           Weiter Lesen
+    //           <svg
+    //             className="ml-2 h-3.5 w-3.5"
+    //             aria-hidden="true"
+    //             xmlns="http://www.w3.org/2000/svg"
+    //             fill="none"
+    //             viewBox="0 0 14 10"
+    //           >
+    //             <path
+    //               stroke="currentColor"
+    //               stroke-linecap="round"
+    //               stroke-linejoin="round"
+    //               stroke-width="2"
+    //               d="M1 5h12m0 0L9 1m4 4L9 9"
+    //             />
+    //           </svg>
+    //         </Button>
+    //       </Link>
+    //     </CardFooter>
+    //     </div>
+    //     {/* <div className="flex items-center justify-center">
+    //       <a
+    //         href={readMoreLink}
+    //         className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    //       >
+
+    //       </a>
+    //     </div> */}
+    //   </div>
+    // </div>
   );
 };
 
--- src/pages/politik/index.tsx
+++ src/pages/politik/index.tsx
@@ -4,11 +4,37 @@ import PoliticsCard from "~/components/PoliticsCard";
 
 const politicsJson = [
   {
-    partyComment: '"...."',
-    partyImage: "night-mode.svg",
-    partyName: "svp",
+    partyComment:
+      '"Eine Strom-Mangellage ist die grösste Bedrohung für unser Land und wird gemäss Bund bereits in naher Zukunft Realität – mit katastrophalen Folgen für die Menschen in der Schweiz."',
+    partyImageSource:
+      "https://www.svp.ch/wp-content/themes/svp2022/images/SVP_Logo_de.png",
+    partyProgramLink: "https://www.svp.ch/positionen/parteiprogramme/",
+    partyImage: "/politik/svp-logo.png",
+    partyName: "SVP",
     linkToParty: "https://www.svp.ch",
-    readMoreLink: "svp",
+    readMoreLink: "politik/svp",
+  },
+  {
+    partyComment:
+      '"Die SP Schweiz sieht die aktuelle Stromversorgungssicherheit als ungenügend, erstens, abhängig von fossilen Energie-Importen; zweitens,unsere AKWs älter werden; drittens, erneuerbare Energien nicht genügend ausgebaut."',
+    partyImageSource:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Logo_der_Sozialdemokratischen_Partei_der_Schweiz_2009%2C_single.svg/1950px-Logo_der_Sozialdemokratischen_Partei_der_Schweiz_2009%2C_single.svg.png",
+    partyProgramLink: "https://www.sp-ps.ch/wofuer-wir-stehen/parteiprogramm/",
+    partyImage: "/politik/sp-logo.png",
+    partyName: "SP",
+    linkToParty: "https://www.sp-ps.ch",
+    readMoreLink: "politik/sp",
+  },
+  {
+    partyComment:
+      'Die EDU sieht die aktuelle Stromversorgungslage kritisch, insbesondere hinsichtlich der Versorgungssicherheit und der Abhängigkeit von fossilen Brennstoffen und Importen.',
+    partyImageSource:
+      "https://www.edu-sh.ch/app/download/8813876386/Logo_EDU-UDF_d_ES_gross.jpg?t=1502204674",
+    partyProgramLink: "https://www.edu-schweiz.ch/positionen/",
+    partyImage: "/politik/edu-logo.jpg",
+    partyName: "EDU",
+    linkToParty: "https://www.edu-schweiz.ch",
+    readMoreLink: "politik/edu",
   },
 ];
 
@@ -18,19 +44,22 @@ const politics = () => {
       <div className="flex">
         <main className="relative flex-grow">
           <Navbar />
-          <div
-            className={`grid grid-cols-1 justify-center md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-4`}
-          >
-            {politicsJson.map((item) => (
-              <PoliticsCard
-                partyComment={item.partyComment}
-                partyImage={item.partyImage}
-                partyName={item.partyName}
-                linkToParty={item.linkToParty}
-                readMoreLink={item.readMoreLink}
-                key={item.partyName}
-              />
-            ))}
+          <div className="mt-4"></div>
+          <div className="col-span-12 mx-2 flex items-stretch justify-center space-x-8 sm:col-span-12 md:col-span-8 2xl:col-span-4">
+            <div className="grid grid-cols-1 justify-center gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3">
+              {politicsJson.map((item) => (
+                <PoliticsCard
+                  partyProgramLink={item.partyProgramLink}
+                  partyImageSource={item.partyImageSource}
+                  partyComment={item.partyComment}
+                  partyImage={item.partyImage}
+                  partyName={item.partyName}
+                  linkToParty={item.linkToParty}
+                  readMoreLink={item.readMoreLink}
+                  key={item.partyName}
+                />
+              ))}
+            </div>
           </div>
         </main>
       </div>
